e6747bbebc6ec472d28e27891043278ab24e933c
xndadelin <adelinmihai071@gmail.com>
2025-07-27 17:05:12 +0300

    init project

---
+++ spec/sponsortrack_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sponsortrack do
+  it "has a version number" do
+    expect(Sponsortrack::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
